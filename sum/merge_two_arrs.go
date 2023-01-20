634269d9ef32b11d576b7d163e5eb01da037ac75
Ernest <musaubrian15@gmail.com>
2023-01-20 12:05:28 +0300

    chore: merge two sorted arrays

---
+++ sum/merge_two_arrs.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	i, j, k := m-1, n-1, m+n-1
+
+	for i >= 0 && j >= 0 {
+		if nums1[i] > nums2[j] {
+			nums1[k] = nums1[i]
+			i--
+		} else {
+			nums1[k] = nums2[j]
+			j--
+		}
+		k--
+	}
+	for j >= 0 {
+		nums1[k] = nums2[j]
+		j--
+		k--
+	}
+	fmt.Println(nums1)
+}
+
+func main() {
+	var nums []int
+	var nums2 []int
+	nums = append(nums, 1, 2, 3, 0, 0, 0)
+	nums2 = append(nums2, 2, 5, 6)
+	merge(nums, 3, nums2, 3)
+}
