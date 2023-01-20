e286fc6a668bf3ad667c905e0a66fcb99e19e15c
Ernest <musaubrian15@gmail.com>
2023-01-20 12:09:42 +0300

    chore: format code

--- search/insert_position.go
+++ search/insert_position.go
@@ -2,21 +2,19 @@ package main
 
 import "fmt"
 
-
 func searchInsert(nums []int, target int) int {
-    for idx, num := range(nums) {
-        if num >= target {
-            return idx
-        }
-    }
-    return len(nums)
+	for idx, num := range nums {
+		if num >= target {
+			return idx
+		}
+	}
+	return len(nums)
 }
-
 
 func main() {
 
-    var arr[] int
-    arr = append(arr, 1,3,5,6)
-    result := searchInsert(arr, 7)
-    fmt.Println(result)
+	var arr []int
+	arr = append(arr, 1, 3, 5, 6)
+	result := searchInsert(arr, 7)
+	fmt.Println(result)
 }
