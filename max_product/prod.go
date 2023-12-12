4565109b47e6ec783a089044a5c99e87326c3c8f
musaubrian <musaubrian15@gmail.com>
2023-12-12 13:48:13 +0300

    cleanup

--- max_product/prod.go
+++ max_product/prod.go
@@ -3,22 +3,7 @@ package main
 import (
 	"fmt"
 	"slices"
-	"sort"
 )
-
-//	func bubbleSort(nums []int) []int {
-//		swapped := true
-//		for swapped {
-//			swapped = false
-//			for i := 1; i < len(nums); i++ {
-//				if nums[i-1] > nums[i] {
-//					nums[i], nums[i-1] = nums[i-1], nums[i]
-//					swapped = true
-//				}
-//			}
-//		}
-//		return nums
-//	}
 
 func maxProduct(nums []int) int {
 	slices.Sort(nums)
