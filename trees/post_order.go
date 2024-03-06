d49cab1abe79f4d89627c7164ed0d3cd0982c316
musaubrian <musaubrian15@gmail.com>
2024-03-06 14:21:36 +0300

    chore: finish up on traversals

---
+++ trees/post_order.go
@@ -0,0 +1,13 @@
+package main
+
+func postorderTraversal(root *TreeNode) []int {
+	var res []int
+	if root == nil {
+		return res
+	}
+
+	res = append(res, postorderTraversal(root.Left)...)
+	res = append(res, postorderTraversal(root.Right)...)
+	res = append(res, root.Val)
+	return res
+}
---
+++ trees/pre_order.go
@@ -0,0 +1,13 @@
+package main
+
+func preorderTraversal(root *TreeNode) []int {
+	var res []int
+	if root == nil {
+		return res
+	}
+
+	res = append(res, root.Val)
+	res = append(res, preorderTraversal(root.Left)...)
+	res = append(res, preorderTraversal(root.Right)...)
+	return res
+}
